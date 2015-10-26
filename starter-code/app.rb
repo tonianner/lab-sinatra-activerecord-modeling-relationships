84791c1603dc54ea106d1443f8eb93a6815fffb4
Toni Anne Romero <tonianne.romero@gmail.com>
2015-10-27 00:03:50 +0800

    Recipe index,show,edit,delete,new working

--- starter-code/app.rb
+++ starter-code/app.rb
@@ -3,4 +3,65 @@ class RecipeApp < Sinatra::Base
   get '/' do
     erb :home
   end
+
+  get '/home' do
+    erb :home
+  end
+
+  # index
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb(:"recipes/index")
+  end
+
+  get '/recipes/new' do
+    @recipe = Recipe.new
+    erb :"recipes/new"
+  end
+
+   # create
+  post '/recipes' do
+    @recipe = Recipe.new(params[:recipe])
+    if @recipe.save
+      redirect("/recipes/#{@recipe.id}")
+    else
+      erb(:"recipes/new")
+    end
+  end
+
+  # show
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb(:"recipes/show")
+  end
+
+    # edit
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb(:"recipes/edit")
+  end
+
+  # given data, update a specific artis
+  # update
+  post '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update_attributes(params[:recipe])
+      redirect("/recipes")
+    else
+      erb(:"recipes/edit")
+    end
+  end
+
+  # delete
+  post '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      redirect('/recipes')
+    else
+      redirect("/recipes/#{@artist.id}/edit")
+    end
+  end
+
 end
+
+
--- starter-code/config.ru
+++ starter-code/config.ru
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
+# Models
 require "./models/course"
 require "./models/ingredient"
 require "./models/recipe"
 
+# Controllers
 require "./app"
 
 run RecipeApp
